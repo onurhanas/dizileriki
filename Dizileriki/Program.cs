06392ec695e46ac92215a0d4d2e14f03597e7f71
onurhanas <113357599+onurhanas@users.noreply.github.com>
2022-10-03 20:10:47 +0300

    dizileriki

---
+++ Dizileriki/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Dizileriki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] sayiDizisi={23,12,4,86,72,3,11,17};
+           foreach (var sayi in sayiDizisi)
+           {
+            Console.WriteLine(sayi);
+           }
+         Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+           {
+            Console.WriteLine(sayi);
+           }
+           Array.Clear(sayiDizisi,2,2);
+           foreach (var sayi in sayiDizisi)
+           {
+            Console.WriteLine(sayi);
+           }
+           Array.Reverse(sayiDizisi);
+           foreach (var sayi in sayiDizisi)
+           {
+            Console.WriteLine(sayi);
+           }
+           
+           Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+           Array.Resize<int>(ref sayiDizisi,9);
+           sayiDizisi[8]=99;
+           foreach (var sayi in sayiDizisi)
+           {
+            Console.WriteLine(sayi);
+            
+           }
+           
+        }
+    }
+}
